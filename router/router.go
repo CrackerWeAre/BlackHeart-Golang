5b086991c7da42bb7931519548cf404c7823d02b
ssoyyoung <ssoyyoung.p@gmail.com>
2020-11-23 22:52:07 +0900

    fix : Allow CORS.

--- router/router.go
+++ router/router.go
@@ -1,13 +1,16 @@
 package router
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+
 	"github.com/ssoyyoung.p/BlackHeart-Golang/handler"
 )
 
 // Router func
 func Router() *gin.Engine {
 	r := gin.Default()
+	r.Use(cors.Default())
 
 	// User API Router
 	userAPI := r.Group("/user")
