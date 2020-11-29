0fed7de41d77a21d13a4bad1f7afb83397f66e05
ssoyyoung <ssoyyoung.p@gmail.com>
2020-11-29 19:16:13 +0900

    etc : Change return value.

---
+++ utils/jsonUtil.go
@@ -0,0 +1,21 @@
+package utils
+
+import "github.com/gin-gonic/gin"
+
+// JSONReturnMsg func
+func JSONReturnMsg(status bool, message string) gin.H {
+
+	return gin.H{
+		"status":  status,
+		"message": message,
+	}
+}
+
+// JSONReturnRes func
+func JSONReturnRes(status bool, result interface{}) gin.H {
+
+	return gin.H{
+		"status": status,
+		"result": result,
+	}
+}
